f8d08336427aab4ec9e9ac7852d638e373551441
Dullah Mtavya <mtavya420@gmail.com>
2023-03-25 17:56:39 +0900

    add map directions

--- src/components/BookingMenu/BookingMenu.js
+++ src/components/BookingMenu/BookingMenu.js
@@ -1,13 +1,10 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
-import Map from "../Map/Map";
+import MapDirections from "../MapDirections/MapDirections";
 import "./BookingMenu.css";
 
 const BookingMenu = ({ onMakeAppointment, onModifyAppointment, onBack }) => {
-
-  const handleViewOnMapClick = () => {
-    window.open(`https://www.google.com/maps/search/?api=1&query=37.5317,127.001`);
-  }
+ 
 
   return (
     <div className="d-flex flex-column px-3 pt-5 pt-md-0 justify-content-md-center booking-page ">
@@ -47,18 +44,7 @@ const BookingMenu = ({ onMakeAppointment, onModifyAppointment, onBack }) => {
               </a>
             </div>
           </div>
-          <div className="mb-2">
-            <Button
-              variant="outline-primary"
-              className="d-block w-100"
-              onClick={handleViewOnMapClick}
-            >
-              View on Map
-            </Button>
-          </div>
-          {/* <div className="">
-            <Map />
-          </div> */}
+          <MapDirections/>
         </div>
       </div>
     </div>
