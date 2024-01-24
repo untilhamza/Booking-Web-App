72d7d8825af04f5206afc884836454c31f8daae2
Sam <samywina@gmail.com>
2024-01-24 11:29:29 +0900

    changed kakaomap and googlemnap address of the shop

--- src/components/MapDirections/MapDirections.tsx
+++ src/components/MapDirections/MapDirections.tsx
@@ -5,10 +5,10 @@ import Button from "react-bootstrap/Button";
 const MapDirections = () => {
 
   const handleViewKakaoMapClick = () => {
-    window.open(`https://map.kakao.com/link/to/Elkpro services,37.531619,127.001225`);
+    window.open(`https://map.kakao.com/link/to/Elkpro services,37.535323741447456, 126.99206905398854`);
   };
   const handleViewGoogleMapClick = () => {
-    window.open(`https://www.google.com/maps/search/?api=1&query=37.531619,127.001225`);
+    window.open(`https://www.google.com/maps/search/?api=1&query=37.535323741447456, 126.99206905398854`);
   };
   return (
     <div className="mb-2 row p-3 gap-2">
